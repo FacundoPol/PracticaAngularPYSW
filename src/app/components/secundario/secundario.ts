c297091da57eaddc9956f9251a82b0b0687988a6
Facu <facundopol24@gmail.com>
2026-05-17 12:53:59 -0300

    Armado de proyecto

---
+++ src/app/components/secundario/secundario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secundario',
+  imports: [],
+  templateUrl: './secundario.html',
+  styleUrl: './secundario.css',
+})
+export class Secundario {}
